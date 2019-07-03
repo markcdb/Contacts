099fcca3e2ab2bc5878e44a3e5b7feb0d7963a20
Mark Christian Buot <mark_cdb@yahoo.com>
2019-07-03 10:59:59 +0800

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Mark Christian Buot on 03/07/2019.
+//  Copyright © 2019 Mark Christian Buot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
